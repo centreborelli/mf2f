d24b8bc5bbc9ed8746dba5b3a3441c452d2a14dc
Valery-Dewil <valery.dewil@ens-cachan.fr>
2020-04-06 18:11:51 +0200

    Offline MF2F without teacher

--- video_f2f_offline_no_teacher.py
+++ video_f2f_offline_no_teacher.py
@@ -373,7 +373,7 @@ def MF2F(**args):
 
         print("Itération = {:03d}, PSNR training stack = {:5.3f}, PSNR eval stack = {:5.3f}, SSIM training stack = {:4.3f}, SSIM eval stack = {:4.3f}   {:1d}".format(i, quant_training_stack, quant_eval_stack, ssim_training, ssim_eval, value))
 
-        iio.write(output_path + "training_{:03d}".format(i), 255*outimg) 
+        iio.write(output_path + "training_{:03d}.png".format(i), 255*outimg) 
         iio.write(args['output']%(i), 255*outimg2) 
 
         list_PSNR_training.append(quant_training_stack)
